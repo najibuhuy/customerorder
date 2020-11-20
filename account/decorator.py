df567ebb7170750647d4c850b4b2b690c82103c8
najibuhuy <48340587+najibuhuy@users.noreply.github.com>
2020-11-20 20:01:12 +0700

    Add files via upload

---
+++ account/decorator.py
@@ -0,0 +1,41 @@
+from django.http import HttpResponse
+from django.shortcuts import redirect
+
+def unauthenticated_user(view_func):
+	def wrapper_func(request, *args, **kwargs):
+		if request.user.is_authenticated:
+			return redirect('homepage')
+		else:
+			return view_func(request, *args, **kwargs)
+
+	return wrapper_func
+
+
+def allowed_users(allowed_roles=[]):
+	def decorators(view_func):
+		def wrapper_func(request, *args, **kwargs):
+			group = None
+			if request.user.groups.exists():
+				group = request.user.groups.all()[0].name
+
+			if group in allowed_roles:
+				return view_func(request, *args, **kwargs)
+
+			else:
+				return HttpResponse('Maaf user tidak diperkenankan membuka halaman ini')
+		return wrapper_func
+	return decorators
+
+def admin_only(view_func):
+	def wrapper_func(request,*args, **kwargs):
+		group = None
+		if request.user.groups.exists():
+			group =  request.user.groups.all()[0].name
+
+		if group == 'admin':
+			return view_func(request, *args, **kwargs)
+
+		if group == 'customer':
+			return redirect('user')
+
+	return wrapper_func
